4ba3f8feaab43166d9519546befcf80284f0637b
shinbian11 <shinbian11@gmail.com>
2022-06-13 10:48:18 +0900

    component에 css 적용

--- src/App.js
+++ src/App.js
@@ -9,8 +9,15 @@ function Header(props) {
 
   // Header의 onSelect 이벤트도 속성이 될 수 있다! 그러므로 props.onSelect 으로 접근이 가능한 것이다.
   // Javascript의 함수가 일급객체인 점을 이용해 props 객체에 함수도 담아서 넘겨줄 수 있다는 내용
+
+  const myStyle = {
+    borderBottom: "1px solid gray",
+    padding: "10px",
+    fontSize: "20px",
+  };
+
   return (
-    <header>
+    <header style={myStyle}>
       <h1>
         <a
           href="/"
