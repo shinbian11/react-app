7fd2996484bd2e0fc4ebfd588a74360845866e77
shinbian11 <shinbian11@gmail.com>
2022-06-15 11:52:42 +0900

    mode에 따른 if~else 문 삭제 (Link와 Router 로 이것을 대신했으므로!)

--- src/App.js
+++ src/App.js
@@ -13,48 +13,15 @@ import { Create } from "./Create";
 function createHandler() {}
 
 function App() {
-  const [mode, setMode] = useState("WELCOME");
-  const [id, setId] = useState(null);
+  const [mode, setMode] = useState("WELCOME"); // todo 삭제 예정
+  const [id, setId] = useState(null); // todo 삭제 예정
   const [nextId, setNextId] = useState(3);
   const [topics, setTopics] = useState([
     { id: 1, title: "html", body: "very very! HTML is ..." },
     { id: 2, title: "css", body: "css is ..." },
   ]);
 
-  let content = null;
-  if (mode === "WELCOME") {
-    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
-  } else if (mode === "READ") {
-    const topic = topics.filter((e) => {
-      if (e.id === id) {
-        return true;
-      } else {
-        return false;
-      }
-    })[0];
-    content = <Article title={topic.title} body={topic.body}></Article>;
-  } else if (mode === "CREATE") {
-    content = (
-      <Create
-        onCreate={(title, body) => {
-          const newTopic = { id: nextId, title, body };
-
-          // topics.push(newTopic);
-          // setTopics(topics);
-          const newTopics = [...topics];
-          newTopics.push(newTopic);
-          setTopics(newTopics);
-          setId((current) => nextId);
-          setNextId((current) => nextId + 1);
-          setMode("READ");
-        }}
-      ></Create>
-    );
-  }
-
-  // 모드의 값이 바뀌었을때, App 함수가 다시 호출된다. 그 return 값이 웹페이지에 반영되게 하고 싶다! ===> state의 개념 도입!
-  // 중요 ***) 리액트에서 상태(state)는 값이 바뀌었을 때 컴포넌트를 다시 실행한다!!
-  // mode = "WELCOME";
+  // 중요) 리액트에서 상태(state)는 값이 바뀌었을 때 컴포넌트를 다시 실행한다!!
   return (
     <div>
       <Header onSelect={headerHandler()}></Header>
@@ -146,17 +113,6 @@ function App() {
     return () => {
       setMode("WELCOME");
     };
-  }
-
-  function Read(props) {
-    const topic = topics.filter((e) => {
-      if (e.id === id) {
-        return true;
-      } else {
-        return false;
-      }
-    })[0];
-    return <Article title={topic.title} body={topic.body}></Article>;
   }
 }
 
